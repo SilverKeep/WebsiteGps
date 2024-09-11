97a6c2e0b046b410f4646b99a2e8dbdbf02c02d2
Chris Lee <leechris0142@students.esuhsd.org>
2024-09-11 16:01:09 +0000

    deleted some randm files

--- index.js
+++ index.js
@@ -8,9 +8,3 @@ router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html')); 
     //__dirname : It will resolve to your project folder. 
 }); 
-router.get('/about', function(req, res) { 
-    res.sendFile(path.join(__dirname + '/about.html')); 
-}); 
-router.get('/sitemap', function(req, res) { 
-    res.sendFile(path.join(__dirname + '/sitemap.html')); 
-}); 
