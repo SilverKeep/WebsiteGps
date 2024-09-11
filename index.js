cc9af7a2bea32b0239a5005f666784ffeb2ba40d
Chris Lee <leechris0142@students.esuhsd.org>
2024-09-10 23:10:31 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import essential libraries 
+const express = require('express'); 
+const app = express(); 
+const path = require('path'); 
+const router = express.Router(); 
+// Setup essential routes 
+router.get('/', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/index.html')); 
+    //__dirname : It will resolve to your project folder. 
+}); 
+router.get('/about', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/about.html')); 
+}); 
+router.get('/sitemap', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/sitemap.html')); 
+}); 
