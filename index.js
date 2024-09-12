c85bd5b5e696622adb22d3afd07db88333abd2a7
Chris Lee <leechris0142@students.esuhsd.org>
2024-09-12 10:02:45 -0700

    ????

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-// Setup essential routes 
-app.get('/', function(req, res) { 
-    app.send(path.join(__dirname + '/index.html')); 
-    //__dirname : It will resolve to your project folder. 
-}); 
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
