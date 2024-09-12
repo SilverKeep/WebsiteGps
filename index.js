b0dd42cd302fde36e54fe641dff10afb15f4c74d
Chris Lee <leechris0142@students.esuhsd.org>
2024-09-12 09:41:32 -0700

    Update index.js to port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'); 
 const app = express(); 
+const port = process.env.PORT || 3000
+
 const path = require('path'); 
 const router = express.Router(); 
 // Setup essential routes 
