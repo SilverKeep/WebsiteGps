224cb507930694905bf451a9055a4e0d0486b881
Chris Lee <leechris0142@students.esuhsd.org>
2024-09-12 10:00:09 -0700

    send file works i think

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 // Setup essential routes 
 app.get('/', function(req, res) { 
-    res.sendFile(path.join(__dirname + '/index.html')); 
+    app.send(path.join(__dirname + '/index.html')); 
     //__dirname : It will resolve to your project folder. 
 }); 
 
