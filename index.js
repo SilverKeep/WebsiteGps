cc6b9a411399488a83a3113e95c31c6c4acd70db
Chris Lee <leechris0142@students.esuhsd.org>
2024-09-12 09:54:50 -0700

    experimental

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const path = require('path'); 
-const router = express.Router(); 
 // Setup essential routes 
-router.get('/', function(req, res) { 
+app.get('/', function(req, res) { 
     res.sendFile(path.join(__dirname + '/index.html')); 
     //__dirname : It will resolve to your project folder. 
 }); 
