2b33595038e4f5e67023cc619cffe72e97f8afe3
Chris Lee <leechris0142@students.esuhsd.org>
2024-09-12 22:24:48 -0700

    Update index.js to hsotname

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const hostName = 'monkfish-app-xmvp8.ondigitalocean.app';
 
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, hostName, () => console.log(`Example app listening on port ${port}!`))
